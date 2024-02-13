484a9730a7223e7058edf440c526e8e0fbb6ac94
leon <leon.seki.tokyo@gmail.com>
2024-02-13 23:05:47 +0900

    conveter-1

---
+++ hera-rest/src/main/java/jp/tokyo/leon/hera/rest/config/CustomWebMvcConfigurer.java
@@ -0,0 +1,29 @@
+package jp.tokyo.leon.hera.rest.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+/**
+ * @author leon
+ * @date 2024/2/13 22:47
+ */
+@Configuration
+public class CustomWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converters.add(converter);
+    }
+}
